420e2322143c27010758d913a2f279c576a20f07
John Carlo Ricana <cier.alteir@gmail.com>
2020-07-26 15:17:39 +0800

    User/Company
    Signed-off-by: John Carlo Ricana <cier.alteir@gmail.com>

---
+++ database/seeds/CompanySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Company\Company;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Company::class, 5)->create()->each(function ($company) {
+            $company->users()->save(factory(User::class)->make());
+        });
+    }
+}
